02fc2ba475d384a668c72b62ade60e5cb2301fca
Alphabets, Alphabets <yapjiahong-p@proton.me>
2025-04-08 16:37:41 +0800

    feat: register user

--- src/main/java/dcoms/Register.java
+++ src/main/java/dcoms/Register.java
@@ -1,8 +1,29 @@
 package dcoms;
 
-public class Register {
-    public static boolean register(String username, char[] password) {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-        return false;
+public class Register {
+    public static boolean register(String username, char[] password) throws SQLException {
+        Connection connection = Database.getConnection();
+        String query = "SELECT username FROM users WHERE username = ?;";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, username);
+
+        ResultSet rs = stmt.executeQuery();
+        if (rs.getRow() != 0) {
+            // TODO: Proper error message.
+            return false;
+        }
+
+        query = "INSERT INTO users (username, password) VALUES (?, ?);";
+        stmt = connection.prepareStatement(query);
+
+        stmt.setString(1, username);
+        stmt.setString(2, new String(password));
+
+        return true;
     }
 }
