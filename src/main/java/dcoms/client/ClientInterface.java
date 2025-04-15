c736dd52b7b687f46524d32f4d8a84a16c3ee9ec
Anson Yeong <ansonyeongsoonlok@gmail.com>
2025-04-15 18:32:20 +0800

    Resolved conflicts and applied changes from master to client-login

--- src/main/java/dcoms/client/ClientInterface.java
+++ src/main/java/dcoms/client/ClientInterface.java
@@ -9,13 +9,32 @@ import dcoms.remote.RemoteInterface;
 
 public class ClientInterface {
     private static String rmiString;
+<<<<<<< Updated upstream
 
     public ClientInterface(String ip, int port) {
         rmiString = "rmi://" + ip + ":" + port + "/";
+=======
+    private static Dotenv dotenv = Env.env;
+    
+    static {
+        if (dotenv != null) {
+            rmiString = "rmi://" + dotenv.get("RMI_IP") + ":" + dotenv.get("RMI_PORT") + "/";
+        }
+    }
+
+    public ClientInterface() {
+        // Constructor is now just for creating instances if needed
+        if (rmiString == null && dotenv != null) {
+            rmiString = "rmi://" + dotenv.get("RMI_IP") + ":" + dotenv.get("RMI_PORT") + "/";
+        }
+>>>>>>> Stashed changes
     }
 
     public static RemoteInterface getFunction(String name)
             throws MalformedURLException, RemoteException, NotBoundException {
+        if (rmiString == null) {
+            throw new IllegalStateException("RMI connection string not initialized. Make sure Env.loadEnv() was called.");
+        }
         return (RemoteInterface) Naming.lookup(rmiString + name);
     }
 }
