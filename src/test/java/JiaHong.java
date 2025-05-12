36911065ec98b733f2075263873e58febcf6ac70
Yap Jia Hong <yapjiahong-p@proton.me>
2025-05-12 09:01:25 +0800

    wip: test cases

---
+++ src/test/java/JiaHong.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class JiaHong {
+    @Test 
+    void getUserRole() {
+        assertEquals(20, 1 + 1);
+    }
+
+    @Test
+    void getQuantity() {
+        
+    }
+}
